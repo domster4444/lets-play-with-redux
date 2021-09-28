241912baca77fafde8c31c0359c5a6a7ae9fba28
Kshitiz <kshitiz.stha11@gmail.com>
2021-09-28 12:26:18 -0400

    using redux

--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,19 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+//wrap with redux wrapper
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import rootReducer from 'service/reducers/index';
+const store = createStore(rootReducer);
+console.log('store from index.js ____________ ', store);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
+
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
